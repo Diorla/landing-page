2d0a66ce16f7139c992ae552f8b8683fe8f3eff7
Ade Adeola <adedotster@gmail.com>
2022-02-19 11:29:06 +0000

    comments

--- js/app.js
+++ js/app.js
@@ -13,12 +13,15 @@ const footer = document.querySelector("footer");
 const topButton = document.getElementById("top");
 const mobileMenuIcon = document.querySelector(".fa-solid.menu-icon");
 
+// utility function: turns "helloWorld" to "hello world"
 const pascalToSentence = (str) =>
   str
     .split("")
     .map((item) => (/[A-Z]/.test(item) ? ` ${item}` : item))
     .join("")
     .trim();
+
+// utility function: turns "helloWorld" to "hello-world"
 const pascalToSnake = (str) =>
   str
     .split("")
@@ -26,6 +29,8 @@ const pascalToSnake = (str) =>
     .join("")
     .toLowerCase()
     .trim();
+
+// create mobile and desktop nav links
 document.addEventListener("DOMContentLoaded", () => {
   let desktopList = `<ul>`;
   let mobileList = `<ul>`;
@@ -67,6 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 let timeoutId;
+// hide desktop navbar upon scrolling down
 document.addEventListener("scroll", () => {
   desktopNav.classList.remove("hidden");
   if (timeoutId) clearTimeout(timeoutId);
@@ -76,6 +82,7 @@ document.addEventListener("scroll", () => {
     }, 1000);
 });
 
+// expand and collapse mobile nav
 mobileMenuIcon.addEventListener("click", () => {
   if (mobileNav.classList.contains("collapsed")) {
     mobileNav.classList.remove("collapsed");
@@ -92,8 +99,8 @@ let options = {
   threshold: 1,
 };
 
+// update the nav item and set to active when section comes into view
 let observer = new IntersectionObserver((obList) => {
-  // Only the first
   const [observeEntry] = obList;
   const { isIntersecting, target } = observeEntry;
 
@@ -115,6 +122,7 @@ observer.observe(pricing);
 observer.observe(contactUs);
 observer.observe(footer);
 
+// scroll to top on clicking button#top
 topButton.addEventListener("click", () => {
   document.body.scrollTo({
     top: 0,
